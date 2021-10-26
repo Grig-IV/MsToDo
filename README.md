4743167d531c3a73a34f3622ad2066d379f9be1d
Grig-IV <77640875+Grig-IV@users.noreply.github.com>
2021-10-26 15:20:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MsToDo
+ Clone of Microsoft To Do created for study purpose (WPF/MVVM)
---
+++ src/MsToDo/Views/MsToDoView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace MsToDo
+{
+    public partial class MsToDoView : Window
+    {
+        public MsToDoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
