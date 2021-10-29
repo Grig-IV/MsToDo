abaf3c810079da1838f2c047176474204d0a51cc
Grig-IV <77640875+Grig-IV@users.noreply.github.com>
2021-10-29 19:03:15 +0300

    Create empty todo list picture, and contextMenu improved

---
+++ src/MsToDo/XamlVectorGraphics/EmptyToDoListPic.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MsToDo.XamlVectorGraphics
+{
+    /// <summary>
+    /// Interaction logic for EmptyToDoListPic.xaml
+    /// </summary>
+    public partial class EmptyToDoListPic : Canvas
+    {
+        public EmptyToDoListPic()
+        {
+            InitializeComponent();
+        }
+    }
+}
